0f23b0c7190a16133cd7e6194c2cd6b2d75327bd
Shuxrat <shuukhratbegmatov@gmail.com>
2024-11-26 16:41:54 +0500

    fix

---
+++ precache-manifest.71974539bbb44d470b67173b602a59be.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85a07ba34049135a8fd",
+    "url": "/css/about.2098d057.css"
+  },
+  {
+    "revision": "cef9004b6dd2de7040b7",
+    "url": "/css/app.b57f8bee.css"
+  },
+  {
+    "revision": "1494ff5c87e40e281ebf",
+    "url": "/css/chunk-vendors.db0db735.css"
+  },
+  {
+    "revision": "e5f18cb987385760e628a9671f975412",
+    "url": "/fonts/Inter-Medium.e5f18cb9.ttf"
+  },
+  {
+    "revision": "188c253e810c9aecfeaa12795d667868",
+    "url": "/img/Logomark.188c253e.svg"
+  },
+  {
+    "revision": "fafae29851b13f230c18d9b9f67ca81c",
+    "url": "/img/dash_bg.fafae298.png"
+  },
+  {
+    "revision": "bc253e7dc46665d761834f926cf83e34",
+    "url": "/img/image_1.bc253e7d.png"
+  },
+  {
+    "revision": "71a76c4237444b0f3b540f0a2c46179c",
+    "url": "/img/login_bg.71a76c42.png"
+  },
+  {
+    "revision": "8f7647756681c8bc70f93e7a3e456a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85a07ba34049135a8fd",
+    "url": "/js/about.fb920e8c.js"
+  },
+  {
+    "revision": "cef9004b6dd2de7040b7",
+    "url": "/js/app.6a8b6d8e.js"
+  },
+  {
+    "revision": "1494ff5c87e40e281ebf",
+    "url": "/js/chunk-vendors.f5b9b359.js"
+  },
+  {
+    "revision": "45bf1ea4b920cd1767540987b08988ba",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
